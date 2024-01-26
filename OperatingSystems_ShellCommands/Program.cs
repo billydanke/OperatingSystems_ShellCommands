e97479bf3ecea57748b6268a2a16d9251a5980ac
billydanke <johnjo490@gmail.com>
2024-01-25 21:37:04 -0600

    Implemented code and readme

--- OperatingSystems_ShellCommands/Program.cs
+++ OperatingSystems_ShellCommands/Program.cs
@@ -1,10 +1,155 @@
-﻿namespace OperatingSystems_ShellCommands
+﻿// Operating Systems Homework 1
+// Aaron Barrett
+
+// Note: See README.txt for more detailed information and discussion
+
+using System;
+using System.Diagnostics;
+
+namespace OperatingSystems_ShellCommands
 {
     internal class Program
     {
+        static void DisplayMenu() {
+            //Console.Clear();
+
+            Console.WriteLine("Operating Systems Homework 1 - Running Shell Commands - Aaron Barrett\n");
+            Console.WriteLine("┌──────────────────────────────────┐");
+            Console.WriteLine("│         Command Options          │");
+            Console.WriteLine("├──────────────────────────────────┤");
+            Console.WriteLine("│ 1. List Directory Contents       │");
+            Console.WriteLine("│ 2. Print Working Directory       │");
+            Console.WriteLine("│ 3. Create New Directory          │");
+            Console.WriteLine("│ 4. Display a Message             │");
+            Console.WriteLine("│ 5. Display file contents         │");
+            Console.WriteLine("│ 6. Display File Tree             │");
+            Console.WriteLine("│ 7. Open New Program              │");
+            Console.WriteLine("│ 8. Exit This Program             │");
+            Console.WriteLine("└──────────────────────────────────┘");
+            Console.WriteLine("");
+        }
+
+        static void PromptContinue()
+        {
+            Console.WriteLine("\n");
+            Console.WriteLine("==================================================================");
+            Console.WriteLine("                    Press any key to continue");
+            Console.WriteLine("==================================================================");
+
+            Console.ReadKey(true); // waits for a keypress before continuing
+        }
+
+        static string RunShellCommand(string command)
+        {
+            Process cmdProcess = new Process(); // Create a process
+
+            cmdProcess.StartInfo.FileName = "cmd.exe"; // Specify that cmd is the process
+            cmdProcess.StartInfo.Arguments = $"/c {command}"; // Set its argument as the desired command
+            cmdProcess.StartInfo.RedirectStandardOutput = true; // Redirect the cmd output to the process output stream for viewing
+            cmdProcess.StartInfo.UseShellExecute = false; // Disable using the OS shell to start the process
+            cmdProcess.StartInfo.CreateNoWindow = true; // Disable the window from being shown during the operation
+
+            cmdProcess.Start(); // Run the process
+
+            string processResult = cmdProcess.StandardOutput.ReadToEnd(); // Get the returned text from the output stream
+            cmdProcess.WaitForExit(); // Halts execution until the process is finished (should be quick)
+            return processResult; // Returns the output text from the command operation
+        }
+
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            while (true)
+            {
+                DisplayMenu();
+
+                Console.Write("Enter Command Number: ");
+                string commandOption = Console.ReadLine();
+
+                string command = ""; // Make a blank command string
+
+                if(commandOption == "1") // List a target directory using dir
+                {
+                    // Prompt the user for a target directory
+                    Console.Write("Enter target directory: ");
+                    string targetDirectory = Console.ReadLine();
+
+                    command = $"dir {targetDirectory}";
+                }
+                else if(commandOption == "2") // Print the current directory using dir
+                {
+                    command = "dir";
+                }
+                else if( commandOption == "3") // Make a new directory at target using mkdir
+                {
+                    Console.WriteLine("(Be aware that this command has no output. Check the directory to ensure completion!)");
+
+                    // Prompt the user for a target directory
+                    Console.Write("Enter desired base directory for placement (w/o final slash): ");
+                    string sourceDirectory = Console.ReadLine();
+
+                    // Prompt the user for new directory name
+                    Console.Write("Enter new directory name: ");
+                    string newDirectory = Console.ReadLine();
+
+                    command = $@"mkdir {sourceDirectory}\{newDirectory}";
+                }
+                else if(commandOption == "4") // Display a message using echo
+                {
+                    // Prompt the user for a message
+                    Console.Write("Enter message: ");
+                    string message = Console.ReadLine();
+
+                    command = $"echo {message}";
+                }
+                else if(commandOption == "5") // Display file contents using type
+                {
+                    // Prompt the user for a target directory
+                    Console.Write("Enter filepath: ");
+                    string filePath = Console.ReadLine();
+
+                    command = $"type {filePath}";
+                }
+                else if(commandOption == "6") // List filetree using tree
+                {
+                    Console.WriteLine("(Be careful with this, as choosing something like C:\\ as root can take a while to complete)");
+                    // Prompt the user for start path
+                    Console.Write("Enter root path: ");
+                    string rootPath = Console.ReadLine();
+
+                    command = $"tree {rootPath}";
+                    Console.WriteLine($"Loading tree from {rootPath}...");
+                }
+                else if(commandOption == "7") // Open a new program at specified path using start
+                {
+                    // Get specified path
+                    Console.Write("Enter program path (if program is on PATH, just enter its name): ");
+                    string programPath = Console.ReadLine();
+
+                    if (programPath != "")
+                    {
+                        command = $"start {programPath}";
+                    }
+                    else
+                    {
+                        command = "start";
+                    }
+                }
+                else if(commandOption == "8") // Exit program
+                {
+                    Console.WriteLine("\nGoodbye!");
+                    Environment.Exit(0);
+                }
+
+                string shellOutput = RunShellCommand(command);
+
+                if(shellOutput != "")
+                {
+                    Console.WriteLine("The command generated the following output:\n");
+                    Console.WriteLine(shellOutput);
+                }
+                Console.WriteLine("Command completed.");
+                PromptContinue();
+            }
         }
     }
 }
---
+++ OperatingSystems_ShellCommands/README.txt
@@ -0,0 +1,31 @@
+﻿----------------------
+    Functionality
+----------------------
+
+This program's functionality is pretty straightforward. You are shown a menu and prompted to pick an option from the list.
+Based on the option you pick, you may be asked to provide additional information, and then a command string is generated.
+From there, the system call is performed based on the string generated.
+
+In C#, to make a system call, you must call the cmd.exe process in the background. To see how that was implemented, see
+the RunShellCommand() function in Program.cs. This function returns an output string from the system call. For instance, if
+the command was "dir", then the output string would be the file list in its entirety separated by newline characters. This
+is then displayed back to the user, and the process is repeated until the user selects quit.
+
+----------------------
+      Discussion
+----------------------
+
+The main part of this section is to discuss the importance of validating user input and handling potential security
+risks associated with using system calls. There are times when giving the user direct access to certain system commands
+can be dangerous, especially if the user does not know that they are capable of harming their system. For instance, if
+the user were to run a delete command and misspell their file path, they may end up permanently deleting the wrong file.
+Displaying confirmation messages before these operations are critical to ensure loss of data does not occur. For certain
+system-critical or otherwise protected files, the win32 delete will require additional confirmation before the deletion
+actually occurs.
+
+For the most part in this program, all options in the selection menu are harmless. There is no ability to delete files,
+and no other available commands in the program are able to modify or delete data. However, there is definitely something
+to be said about the ABILITY of software programs to run shell commands. Even without running as administrator, there
+would benothing stopping a bad actor from writing code to begin deleting as many files in the user's C: drive as possible.
+Even deletion confirmation messages can be overridden with an extra flag when calling the command. For this reason it is
+important for the user to know that the software they are running is safe.
